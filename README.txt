b1c590b95c51c79d4745a257bdb49786b4331ce1
rgueta <ricardogueta@hotmail.com>
2022-04-06 17:06:22 -0700

    Socket.js changed setup to heroku url deployed

---
+++ README.txt
@@ -0,0 +1,1 @@
+This vresion has changes for herocu deployment
--- src/components/Socket.js
+++ src/components/Socket.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 
 console.log('socket-client connected');
-let socket = io('https://localhost:5000');
+// let socket = io('https://localhost:5000');
+let socket = io('https://byh16n-2-4-1.herokuapp.com');
 
 export default socket;
