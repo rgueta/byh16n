a83c6fb482b4d5bdf44416e5b996960df36e0210
rgueta <ricardogueta@hotmail.com>
2022-05-24 19:49:07 -0700

    testing codeEvent

--- src/routes/codeEvents.routes.js
+++ src/routes/codeEvents.routes.js
@@ -3,7 +3,8 @@ import * as codeEventsCtrl from '../controllers/codeEvents.controller';
 import {authJwt}  from "../middleware";
 const router = Router();
 
-router.post('/:codeId/:picId/:CoreSim',codeEventsCtrl.createCode_event);
+// router.post('/:codeId/:picId/:CoreSim',codeEventsCtrl.createCode_event);
+router.post('/',codeEventsCtrl.createCode_event);
 router.get('/:CoreSim',[authJwt.verifyToken,authJwt.isNeighbor],codeEventsCtrl.getCode_events);
 // router.get('/:userId/:CoreSim/:start/:end',[authJwt.verifyToken,authJwt.isNeighbor],codeEventsCtrl.getCode_eventsByDate);
 router.get('/:userId/:CoreSim/:start/:end',codeEventsCtrl.getCode_eventsByDate);
