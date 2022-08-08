6601f143244056c1c41503f2b637e6874a80af34
Gueta <ricardogueta@hotmail.com>
2022-08-08 12:25:35 -0700

    add codeEvent/coreSim root

--- src/routes/codeEvents.routes.js
+++ src/routes/codeEvents.routes.js
@@ -6,7 +6,7 @@ const router = Router();
 router.post('/:codeId/:picId/:CoreSim',codeEventsCtrl.createCode_event);
 // router.post('/',codeEventsCtrl.createCode_event);
 // router.get('/:CoreSim',[authJwt.verifyToken,authJwt.isNeighbor],codeEventsCtrl.getCode_events);
-router.get('/:CoreSim',codeEventsCtrl.getCode_events);
+router.get('/coreSim/:CoreSim',codeEventsCtrl.getCode_events);
 // router.get('/:userId/:CoreSim/:start/:end',[authJwt.verifyToken,authJwt.isNeighbor],codeEventsCtrl.getCode_eventsByDate);
 router.get('/:userId/:CoreSim/:start/:end',codeEventsCtrl.getCode_eventsByDate);
 
