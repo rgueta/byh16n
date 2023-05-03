36eae131db341b20548e56519a5f3be8b673a32a
Gueta <ricardogueta@hotmail.com>
2023-05-03 09:08:00 -0700

    removed two code added for socket session test added before change to socket-session branch

--- src/index.js
+++ src/index.js
@@ -48,30 +48,6 @@ httpServer.listen(PORT);
 
 
 //#region ----- sockets -------------------------------------------------------
-io.use((socket, next) => {
-  const sessionID = socket.handshake.auth.sessionID;
-  if (sessionID) {
-    // find existing session
-    const session = sessionStore.findSession(sessionID);
-    if (session) {
-      socket.sessionID = sessionID;
-      socket.userID = session.userID;
-      socket.username = session.username;
-      return next();
-    }
-  }
-  const username = socket.handshake.auth.username;
-  if (!username) {
-    return next(new Error("invalid username"));
-  }
-  // create new session
-  socket.sessionID = randomId();
-  socket.userID = randomId();
-  socket.username = username;
-  next();
-});
-
-
 
 io.sockets.on("connect_error", (err) => {
   console.log(`connect_error due to ${err.message}`);
@@ -81,11 +57,6 @@ io.sockets.on('connection', async (socket) => {
   await console.log('-------------------------- sockets  ------------------------')
 
   console.log('New connection: ', socket.id + ' ' + new Date().toLocaleString());
-
-  socket.emit("session", {
-    sessionID: socket.sessionID,
-    userID: socket.userID,
-  });
 
   app.use("/api/alert/:room/:title/:msg", async (req, res) => {
     console.log('Si entro al router de alertas..!');
