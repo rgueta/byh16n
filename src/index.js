e21103c118d005ffd11c681a7b92e49e83cb40b7
Gueta <ricardogueta@hotmail.com>
2023-06-09 10:44:16 -0700

    axios working ok

--- src/index.js
+++ src/index.js
@@ -12,6 +12,33 @@ const httpServer = new Server(app,);
 httpServer.listen(PORT);
 // #endregion ------ http  ---------
 
-
 console.log('Server listen on port: ', PORT);
 
+// Firebase ALerts  -----------------
+app.use("/api/alerts/:core/:msg", async (req, res, next) => {
+
+    const config = {
+        method: 'POST',
+        url: process.env.FIREBASE_URI,
+        headers: {
+            'content-type': 'application/json',
+            'Authorization':  'key=' + process.env.FIREBASE_API_KEY
+        },
+        data:  JSON.stringify({to: `/topics/${req.params.core}`, notification: {
+            body: req.params.msg,
+            title: 'Firebase-postman title',
+            subtitle: 'Firebase-postman subtitle'
+        }})
+    }
+
+    let resp = '';
+    await axios(config)
+    .then((result) => {
+        resp = result.data
+        // console.log('OK: ', result)
+    })
+    .catch((err) => console.log('axios error: ', err));
+
+   return await res.status(200).send(resp);
+
+ });
