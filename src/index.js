c14707ad1ef3c22c79b188a55c3efc28174f7334
Gueta <ricardogueta@hotmail.com>
2023-06-09 08:51:28 -0700

    installed axios

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 //--- this file is just to run the app
 import app from './app';
+import { Server } from "http";
+import axios from "axios";
+
 
 // index setup
 const PORT = process.env.PORT;
 
-
 // #region   -------- http  -----------------------------
-import { Server } from "http";
-
 const httpServer = new Server(app,);
-
 httpServer.listen(PORT);
 // #endregion ------ http  ---------
 
 
 console.log('Server listen on port: ', PORT);
-
-
 
